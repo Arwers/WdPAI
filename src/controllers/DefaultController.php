b861a7bdf39a50f7e75f7a6ba66195ab5efbd8dd
Marcin Fortuna <marcin.fortuna@onet.eu>
2025-01-30 20:05:19 +0100

    feat: add controllers

--- src/controllers/DefaultController.php
+++ src/controllers/DefaultController.php
@@ -4,12 +4,10 @@ require_once 'AppController.php';
 
 class DefaultController extends AppController {
     public function index() {
-        //TODO display login.html
-        die("index method");
+        $this->render('login');
     } 
 
     public function tracker() {
-        //TODO display tracker.html
-        die("tracker method");
+        $this->render('tracker');
     }
 }
