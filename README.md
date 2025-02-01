67a88dea4dc2e048a6176b8619830ece150c1593
Marcin Fortuna <marcin.fortuna@onet.eu>
2025-02-01 18:04:00 +0100

    feat: add documentation

---
+++ README.md
@@ -0,0 +1,82 @@
+# Finance tracker
+Web app created with PHP as a final project of a university course.
+
+## Table of contents
+* [General info](#general-info)
+* [Technologies](#technologies)
+* [Setup](#setup)
+* [Features](#features)
+* [Status](#status)
+* [Sources](#sources)
+
+## General info
+Finance Tracker is a web application designed to help users manage their budget and track expenses. 
+The project features role-based authentication, allowing both regular users and administrators to interact with the system. 
+Regular users can add, view, and track expenses, while administrators have access to an admin panel to manage users.
+
+## Technologies
+- **PHP** – The main programming language used for the server-side logic.
+- **PostgreSQL** – Database management system to store user information, expenses, and roles.
+- **HTML/CSS** – For creating the user interface.
+- **JavaScript (Fetch API)** – For asynchronous communication with the backend (e.g., adding expenses without page reload).
+
+
+## Setup
+1. **Clone the Repository:**
+   ```bash
+   git clone https://your-repository-url.git
+   cd finance-tracker
+   ```
+
+1. **turn on docker container:**
+   ```bash
+   docker compose-up -d
+   ```
+
+2. **Launch website:**
+    ```bash
+        http://localhost:8081/index
+    ```
+
+3. **Accounts for testing:**
+    - **Admin Account:**
+        - **Email:** admin@example.com
+        - **Password:** admin123
+    - **Client Account:**
+        - **Email:** client@example.com
+        - **Password:** client123
+    
+## Features
+### Implemented (PC view):
+- **Login page:**  
+  ![Login Page](docs/views/login.png)
+
+- **Registration form:**  
+  ![Registration Form](docs/views/register.png)
+
+- **Finance tracker:**  
+  ![Finance Tracker](docs/views/tracker.png)
+
+- **Admin Panel:**  
+  ![Admin Panel](docs/views/admin_panel.png)
+
+### Implemented (Mobile view):
+- **Login page:**  
+  ![Login Page](docs/views/login_mobile.png)
+
+- **Registration form:**  
+  ![Registration Form](docs/views/register_mobile.png)
+
+- **Finance tracker:**  
+  ![Finance Tracker](docs/views/tracker_mobile.png)
+
+- **Admin Panel:**  
+  ![Admin Panel](docs/views/admin_panel_mobile.png)
+
+
+## Status
+#### 🟢 Active
+Project is in active development
+
+## Sources
+* [WdPAI course](https://www.youtube.com/watch?v=SLxf1ZhlqOQ&list=PLtx5mWTs0kiWv_JaaRF1OfCvg-aFgl18z)
---
+++ docs/database.sql
@@ -0,0 +1,151 @@
+create database wdpai
+    with owner avnadmin;
+
+create table public.role
+(
+    id_role   serial
+        primary key,
+    role_name varchar(50) not null
+);
+
+alter table public.role
+    owner to avnadmin;
+
+create table public.user_details
+(
+    id      serial
+        primary key,
+    name    varchar(100) not null,
+    surname varchar(100) not null
+);
+
+alter table public.user_details
+    owner to avnadmin;
+
+create table public."user"
+(
+    id              serial
+        primary key,
+    id_user_details integer      not null
+        constraint fk_user_userdetails
+            references public.user_details
+            on delete cascade,
+    email           varchar(255) not null,
+    password        varchar(255) not null
+);
+
+alter table public."user"
+    owner to avnadmin;
+
+create table public.user_role
+(
+    id_user integer not null
+        constraint fk_userrole_user
+            references public."user"
+            on delete cascade,
+    id_role integer not null
+        constraint fk_userrole_role
+            references public.role
+            on delete cascade,
+    primary key (id_user, id_role)
+);
+
+alter table public.user_role
+    owner to avnadmin;
+
+create table public.types
+(
+    id_type   serial
+        primary key,
+    type_name varchar(100) not null
+);
+
+alter table public.types
+    owner to avnadmin;
+
+create table public.expenses
+(
+    id      serial
+        primary key,
+    id_user integer        not null
+        constraint fk_expenses_user
+            references public."user"
+            on delete cascade,
+    id_type integer        not null
+        constraint fk_expenses_type
+            references public.types
+            on delete restrict,
+    date    date           not null,
+    name    varchar(255)   not null,
+    price   numeric(10, 2) not null
+);
+
+alter table public.expenses
+    owner to avnadmin;
+
+create view public.view_user_expenses(user_id, first_name, last_name, expense_name, price, date, type_name) as
+SELECT u.id       AS user_id,
+       ud.name    AS first_name,
+       ud.surname AS last_name,
+       e.name     AS expense_name,
+       e.price,
+       e.date,
+       t.type_name
+FROM expenses e
+         JOIN "user" u ON e.id_user = u.id
+         JOIN user_details ud ON u.id_user_details = ud.id
+         JOIN types t ON e.id_type = t.id_type;
+
+alter table public.view_user_expenses
+    owner to avnadmin;
+
+create view public.view_user_roles(user_id, first_name, last_name, role_name) as
+SELECT u.id       AS user_id,
+       ud.name    AS first_name,
+       ud.surname AS last_name,
+       r.role_name
+FROM "user" u
+         JOIN user_details ud ON u.id_user_details = ud.id
+         JOIN user_role ur ON ur.id_user = u.id
+         JOIN role r ON ur.id_role = r.id_role;
+
+alter table public.view_user_roles
+    owner to avnadmin;
+
+create function public.get_user_total_expenses(p_user_id integer) returns numeric
+    language plpgsql
+as
+$$
+DECLARE
+    total NUMERIC(10,2);
+BEGIN
+    SELECT COALESCE(SUM(price), 0)
+    INTO total
+    FROM expenses
+    WHERE id_user = p_user_id;
+
+    RETURN total;
+END;
+$$;
+
+alter function public.get_user_total_expenses(integer) owner to avnadmin;
+
+create function public.check_expense_price() returns trigger
+    language plpgsql
+as
+$$
+BEGIN
+    IF NEW.price < 0 THEN
+        RAISE EXCEPTION 'Price cannot be negative!';
+    END IF;
+    RETURN NEW;
+END;
+$$;
+
+alter function public.check_expense_price() owner to avnadmin;
+
+create trigger trig_check_expense_price
+    before insert or update
+    on public.expenses
+    for each row
+execute procedure public.check_expense_price();
